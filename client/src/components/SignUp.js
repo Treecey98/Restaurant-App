1c601277e7de8efd391bb1ef81214f8e3ec21add
Edward Treece <ed@edwards-mbp.home>
2024-03-25 19:16:57 +0000

    added onChange functions to sign up page

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,7 +1,16 @@
 import '../index.css'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react';
 
 function SignUp() {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [address1, setAddress1] = useState('');
+    const [address2, setAddress2] = useState('');
+    const [postcode, setPostcode] = useState('');
+
 
     let navigate = useNavigate();
 
@@ -10,17 +19,47 @@ function SignUp() {
             <h2 className="signup-title">Sign Up</h2>
             <div className="signup-inputs">
                 <label>Name</label> 
-                <input type="text" />
+                <input 
+                    type="text"
+                    onChange={(e) => {
+                        setName(e.target.value)
+                    }}
+                />
                 <label>Email</label>
-                <input type="text" />
+                <input 
+                    type="text"
+                    onChange={(e) => {
+                        setEmail(e.target.value)
+                    }}
+                />
                 <label>Password</label>
-                <input type="text" />
+                <input 
+                    type="text"
+                    onChange={(e) => {
+                        setPassword(e.target.value)
+                    }} 
+                />
                 <label>First line of address</label>
-                <input type="text" />
+                <input 
+                    type="text"
+                    onChange={(e) => {
+                        setAddress1(e.target.value)
+                    }}
+                />
                 <label>Second line of address</label>
-                <input type="text" />
+                <input 
+                    type="text"
+                    onChange={(e) => {
+                        setAddress2(e.target.value)
+                    }} 
+                />
                 <label>Postcode</label>
-                <input type="text" />
+                <input 
+                    type="text"
+                    onChange={(e) => {
+                        setPostcode(e.target.value)
+                    }}
+                />
 
                 <button className="signup-btn" onClick={() => navigate('/home')}>Sign Up</button>
             </div>
