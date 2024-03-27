63b4f8f60c978932c551f0520c20cb6f8ce51235
Edward Treece <ed@edwards-mbp.home>
2024-03-27 17:22:57 +0000

    styling for user profile

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -2,6 +2,7 @@ import '../index.css'
 import { useNavigate, useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react'
 import Axios from 'axios';
+import EditIcon from '@mui/icons-material/Edit';
 
 function User() {
 
@@ -33,19 +34,35 @@ function User() {
             </div> 
 
             <div className="user-container">
-                <h1 className="user-profile-title">User Profile</h1>
+                <h1 className="user-profile-title">{user.fullname}'s Profile</h1>
                 
-                <div className='user-details e-disabled'>
-                    
-                    <ul>
-                        <li>Name: <input type="text" value={user.fullname} disabled={true} /> </li>
-                        <li>Email: <input type="text" value={user.email} disabled={true} /> </li>
-                        <li>Address: <input type="text" value={fulladdress} disabled={true} /> </li>
-                    </ul> 
+                <div className="user-details-container">
+                    <div className="e-disabled">
+                        
+                        <ul>
+                            <div className='user-details'>
+                                <li>Name: <input type="text" value={user.fullname} disabled={true} /></li>
+                                <EditIcon style={{ fontSize: 25, color: "#80A1C1"}}>Filled</EditIcon>
+                            </div>
 
+                            <div className='user-details'>
+                                <li>Email: <input type="text" value={user.email} disabled={true} /></li>
+                                <EditIcon style={{ fontSize: 25, color: "#80A1C1"}}>Filled</EditIcon>
+                            </div>
+                            
+                            <div className='user-details'>
+                                <li>Address: <input type="text" value={fulladdress} disabled={true} /></li>
+                                <EditIcon style={{ fontSize: 25, color: "80A1C1"}}>Filled</EditIcon>
+                            </div> 
+                            
+                            
+                        </ul> 
+                    
+                    </div>
                     <button className="user-profile-edit-btn">Save details</button>
                 </div>
             </div>
+            
         </>
     )
 }
