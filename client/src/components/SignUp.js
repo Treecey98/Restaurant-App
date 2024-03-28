6693c99439346ed28ced46185e35c303e290fb25
Edward Treece <ed@edwards-mbp.home>
2024-03-28 18:25:35 +0000

    re-structured sign up form using react-form-hook to validate

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,83 +1,43 @@
 import '../index.css'
 import { useNavigate } from 'react-router-dom'
-import { useState } from 'react';
+import { useForm } from 'react-hook-form'
 import Axios from 'axios';
 
 function SignUp() {
 
     let navigate = useNavigate();
 
-    const [fullname, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [address1, setAddress1] = useState('');
-    const [address2, setAddress2] = useState('');
-    const [postcode, setPostcode] = useState('');
+    const { register, handleSubmit }= useForm();
 
-    const register = () => {
-        Axios.post("http://localhost:3001/register", {
-            fullname: fullname,
-            email: email,
-            password: password,
-            address1: address1,
-            address2: address2,
-            postcode: postcode,
-        })
+    const onSubmit = (data) => {
+        Axios.post("http://localhost:3001/register", (data))
 
-        navigate('/home')
-    } 
+        navigate('/')
+    }
 
-    return(
+    return (
         <div className="signup-container">
             <h2 className="signup-title">Sign Up</h2>
-            <div className="signup-inputs">
-                <label>Name</label> 
-                <input 
-                    type="text"
-                    onChange={(e) => {
-                        setName(e.target.value)
-                    }}
-                />
-                <label>Email</label>
-                <input 
-                    type="text"
-                    onChange={(e) => {
-                        setEmail(e.target.value)
-                    }}
-                />
-                <label>Password</label>
-                <input 
-                    type="text"
-                    onChange={(e) => {
-                        setPassword(e.target.value)
-                    }} 
-                />
-                <label>First line of address</label>
-                <input 
-                    type="text"
-                    onChange={(e) => {
-                        setAddress1(e.target.value)
-                    }}
-                />
-                <label>Second line of address</label>
-                <input 
-                    type="text"
-                    onChange={(e) => {
-                        setAddress2(e.target.value)
-                    }} 
-                />
-                <label>Postcode</label>
-                <input 
-                    type="text"
-                    onChange={(e) => {
-                        setPostcode(e.target.value)
-                    }}
-                />
+                <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="signup-inputs">
+                    <label>Name*</label> 
+                    <input {...register("fullname", { required: true})} />
+                    <label>Email*</label>
+                    <input {...register("email", { required: true})} />
+                    <label>Password*</label>
+                    <input {...register("password", { required: true})} />
+                    <label>First line of address*</label>
+                    <input {...register("address1", { required: true})} />
+                    <label>Second line of address*</label>
+                    <input {...register("address2", { required: true})} />
+                    <label>Postcode*</label>
+                    <input {...register("postcode", { required: true})} />
 
-                <button className="signup-btn" onClick={() => register() }>Sign Up</button>
-            </div>
+                    <input className="signup-btn" type="submit" />
+                </div>
+                </form>
         </div>
-    )
-}
+        )
 
+    }
 export default SignUp
