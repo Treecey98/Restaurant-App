d5b89e373d9f793157ccf66e8834a5935f859bfc
Edward Treece <ed@edwards-mbp.home>
2024-04-19 11:35:28 +0100

    linking prod db to server

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config();
+
 
 const db = mysql.createConnection({
-   user: 'root',
-   host: 'localhost',
-   password: 'Boothman',
-   database: 'RestaurantApp'
+   host: process.env.DB_HOST,
+   user: process.env.DB_USERNAME,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_DBNAME
 });
 
 module.exports = db
